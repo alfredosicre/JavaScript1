37090de39cfe9a2380b6a1507aac378c18c99074
alfredosicre <alfredo.sicre@gmail.com>
2025-03-18 13:14:27 +0100

    act04

--- bucles/buclesfor/buclefor16.js
+++ bucles/buclesfor/buclefor16.js
@@ -6,16 +6,16 @@ const read = require('/home/node/leer.js');
 // muestre por pantalla si es un número primo o no.
 
 let num = Number(read("Ingrese número entero positivo mayor que 2: "));
-let condicion = 0;
+let esprimo = true;
 
 if (num > 2){
 
-    for (let i = 2; i <= Math.sqrt(num) && condicion == 0; i++) {
+    for (let i = 2; i <= Math.sqrt(num) && esprimo; i++) {
         // Si num es divisible por i, no es primo
         if (num % i == 0){ // no es primo
         }else{
             console.log(`El número ${num} es un número primo`); // si es primo
-            condicion = 1;
+            esprimo = false;
         }
     }
     
