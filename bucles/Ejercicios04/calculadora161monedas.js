b518d18c42fc89e2a36d5e790ddc4c87b4f65cd5
alfredosicre <alfredo.sicre@gmail.com>
2025-03-21 19:31:01 +0100

    11

--- bucles/Ejercicios04/calculadora161monedas.js
+++ bucles/Ejercicios04/calculadora161monedas.js
@@ -7,26 +7,27 @@ const read = require('/home/node/leer.js');
 //darse como cambio de tal forma que el número total de monedas sea mínimo.
 
 main();
-let correcto = true, salir = 0;
-let precio = 0, dinero = 0, cambio = 0.0;
 
 function main(){
+let correcto = true;
+let precio = 0, dinero = 0, cambio = 0.0;
     
 do{  
 precio = Number(read("Ingrese precio: "));
 dinero = Number(read("Ingrese dinero pagado: "));
-salir = Number(read("Salir ingrese 1: "));
-if(salir = 1){
+
+if(precio == 0){
         correcto = false; // si salir = 1 sale del bucle
-        }
+        }else{
         calcularCambio(precio, dinero);
+        }
 } while (correcto);
     
 }
 
-function calcularCambio(precio, cantidadDinero) {
+function calcularCambio(precio, dinero) {
         // Convertimos el precio y la cantidad de dinero a céntimos para evitar problemas de precisión con decimales
-        let cambio = Math.round((cantidadDinero - precio) * 100); // Multiplicamos por 100 para trabajar con céntimos
+        let cambio = Math.round((dinero - precio) * 100); // Multiplicamos por 100 para trabajar con céntimos
         let billetesYMonedas = [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]; // Billetes y monedas disponibles en céntimos
         let cantidadBilletesYMonedas = []; // Array para almacenar el número de cada billete o moneda a devolver
     
@@ -40,8 +41,8 @@ function calcularCambio(precio, cantidadDinero) {
         for (let i = 0; i < billetesYMonedas.length; i++) {
             let dinero = Math.floor(cambio / billetesYMonedas[i]); // Cuántos billetes o monedas de este tipo podemos dar
             if (dinero > 0) {
-                let tipo = billetesYMonedas[i] >= 100 ? `billetes de ${billetesYMonedas[i] / 100}€` : `monedas de ${billetesYMonedas[i] / 100}€`;
-                cantidadBilletesYMonedas.push(`${cantidad} ${tipo}`); // Guardamos la cantidad de billetes o monedas
+                let tipo = billetesYMonedas[i] >= 500 ? `billetes de ${billetesYMonedas[i] / 100}€` : `monedas de ${billetesYMonedas[i] / 100}€`;
+                cantidadBilletesYMonedas.push(`${dinero} ${tipo}`); // Guardamos la cantidad de billetes o monedas
                 cambio -= dinero * billetesYMonedas[i]; // Restamos el valor del cambio
             }
         }
