2cd3c5b60236af857c4d51ac096b8797721085b5
alfredosicre <alfredo.sicre@gmail.com>
2025-03-21 13:44:06 +0100

    06

--- bucles/Ejercicios04/calculadora16monedas.js
+++ bucles/Ejercicios04/calculadora16monedas.js
@@ -7,7 +7,7 @@ const read = require('/home/node/leer.js');
 //darse como cambio de tal forma que el número total de monedas sea mínimo.
 main();
 
-let correcto = true;
+let correcto = true, salir = 0;
 let precio = 0, dinero = 0, cambio = 0.0;
 
 function main(){
@@ -15,11 +15,24 @@ function main(){
 do{  
         precio = Number(read("Ingrese precio: "));
         dinero = Number(read("Ingrese dinero pagado: "));
+        salir = Number(read("Salir ingrese 1: "));
+        if(salir = 1){
+                correcto = false; // si salir = 1 sale del bucle
+        }else{
         // calculo cuantos euros le tengo que devolver
         cambio = dinero - precio; // total euros y centimos que debo devolver
-        cambio = cambio * 100; // veo los centimos totales que necesito
-        
+        cambio = cambio * 100; // veo los centimos totales que necesito de cambio
+//    
+        // Calcular el número de monedas
+        for (let i = 0; i < cambio; i++) {
+            let cantidadMonedas = Math.floor(cambio);
+            if (cantidadMonedas > 0) {
+                cambio -= cantidadMonedas * monedas;
+            }
+        }
+        }
 
+//
 } while (correcto);
 
 }
